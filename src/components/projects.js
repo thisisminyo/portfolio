4a8ed7bcdf2076dc31f2c509d284a7c63c7a8023
minyo <005snail@gmail.com>
2019-01-17 18:47:33 +0900

    make sample cards for category

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
@@ -10,7 +10,56 @@ class Projects extends Component {
   toggleCategories() {
     if (this.state.activeTab == 0) {
       return (
-        <div><h1>This is React</h1></div>
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: "#fff", height: "176px", background: "url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover" }}>
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat ligula nec mi semper viverra.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: "#fff", height: "176px", background: "url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover" }}>
+              React Project #2
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat ligula nec mi semper viverra.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: "#fff", height: "176px", background: "url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover" }}>
+              React Project #3
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat ligula nec mi semper viverra.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       )
     } else if (this.state.activeTab === 1) {
       return (
@@ -36,9 +85,13 @@ class Projects extends Component {
           <Tab>React3</Tab>
           <Tab>React4</Tab>
         </Tabs>
-        <section className="projects-grid">
-          {this.toggleCategories()}
-        </section>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">
+              {this.toggleCategories()}
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
