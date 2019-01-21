d4a5a30511f5e93d63eb466346e68162691c7b17
minyo <005snail@gmail.com>
2019-01-21 19:52:51 +0900

    resume page - update Skills component

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Grid, Cell } from 'react-mdl';
 import Education from './education'
 import Experience from './experience'
+import Skills from './skills'
 
 class Resume extends Component {
   render() {
@@ -67,7 +68,9 @@ class Resume extends Component {
             />
             <hr style={{ borderTop: '3px solid #e22947' }} />
             <h2>Skills</h2>
+            <Skills
 
+            />
           </Cell>
         </Grid>
       </div>
