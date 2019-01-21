03050f244440534689294b3146d5d7843778ffb3
minyo <005snail@gmail.com>
2019-01-21 18:53:35 +0900

    contact page - add phone, email, skypeID

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
   render() {
@@ -15,7 +15,32 @@ class Contact extends Component {
             />
             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt a dui eget semper. Duis posuere quam porta, egestas purus id, sodales diam. Donec vel fringilla turpis. Etiam molestie laoreet nulla, quis euismod lacus. In bibendum ultricies mauris non vulputate. Fusce vel ornare sem.</p>
           </Cell>
-          <Cell col={6}>half page</Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (123) 456-7890
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    mnngcreator@gmail.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fab fa-skype" aria-hidden="true" />
+                    MySkypeId
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
         </Grid>
       </div>
     );
